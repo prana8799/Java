065c5ef4a2957f1c8e6bba9d15218cb52efa2b35
Pranesh Bala Murugan M <96689686+prana8799@users.noreply.github.com>
2024-06-10 15:42:20 +0530

    Create ToPrint54321injava.java

---
+++ ToPrint54321injava.java
@@ -0,0 +1,16 @@
+public class PrintNumbers {
+
+	public static void main(String[] args)  {
+
+  
+            for(int i=5;i>=1;i--)
+            {
+            	System.out.println(i);
+            }
+            
+            for(int j=5;j<=1;j++)
+            {
+            	System.out.println(j);
+            }
+            
+	}
