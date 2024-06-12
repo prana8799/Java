361a077308138b02332e73b7d0d9d90952db58c7
Pranesh Bala Murugan M <96689686+prana8799@users.noreply.github.com>
2024-06-12 19:37:45 +0530

    Create Differentdatatypes.java

---
+++ Differentdatatypes.java
@@ -0,0 +1,30 @@
+public class DifferentDataTypes {
+
+	public static void main(String[] args) {
+		
+		String name="Pranesh";
+		
+		Integer id=87;
+		
+		double pin=641035;
+		
+		float marks=877.50f;
+		
+		char initial='m';
+		
+		boolean heisagoodboy=true; 
+		
+		System.out.println("His name is:"+name);
+		
+		System.out.println("His id is:"+id);
+		
+		System.out.println("His pincode is:"+pin);
+		
+		System.out.println("His marks is:"+marks);
+		
+		System.out.println("His initial is:"+initial);
+		
+		System.out.println("Is he a good boy? =" + heisagoodboy);
+	}
+
+}
